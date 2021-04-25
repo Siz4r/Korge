aa35707b08ed5d2068fe793dee611b1c99dd1e64
Siz4r <zaq134215@gmail.com>
2021-04-25 10:55:09 +0200

    Lab6

---
+++ Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿
+namespace Lab6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Lab6.Properties.Resources.kwiatek;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(340, 424);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.chart1);
+            this.panel1.Controls.Add(this.trackBar1);
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1232, 650);
+            this.panel1.TabIndex = 1;
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(358, 347);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "red";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "green";
+            series3.ChartArea = "ChartArea1";
+            series3.Legend = "Legend1";
+            series3.Name = "blue";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Series.Add(series3);
+            this.chart1.Size = new System.Drawing.Size(509, 300);
+            this.chart1.TabIndex = 4;
+            this.chart1.Text = "chart1";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(522, 51);
+            this.trackBar1.Maximum = 126;
+            this.trackBar1.Minimum = -126;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(157, 45);
+            this.trackBar1.TabIndex = 2;
+            this.trackBar1.Value = 1;
+            this.trackBar1.ValueChanged += new System.EventHandler(this.trackBar1_ValueChanged);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Lab6.Properties.Resources.kwiatek;
+            this.pictureBox2.Location = new System.Drawing.Point(873, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(347, 443);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(557, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 25);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Kontrast";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1232, 650);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab6
+{
+    public partial class Form1 : Form
+    {
+        int red, green, blue;
+        Bitmap podstawa, zmodyfikowany;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            podstawa = new Bitmap(pictureBox1.Image);
+            zmodyfikowany = new Bitmap(pictureBox2.Image);
+        }
+
+        private void trackBar1_ValueChanged(object sender, EventArgs e)
+        {
+            przeksztalc(trackBar1.Value);
+            obliczHistogram();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            przeksztalc(50);
+        }
+
+        private void przeksztalc(int delta)
+        {
+            Color cModified;
+
+            for(int i  = 0; i < podstawa.Height; i++)
+            {
+                for(int j = 0; j < podstawa.Width; j++)
+                {
+                    Color c = podstawa.GetPixel(j, i);
+                    if (delta >= 0)
+                    {
+                        red = zmianaKontrastuDlaDodatnich(c.R, 127  - delta);
+                        green = zmianaKontrastuDlaDodatnich(c.G, 127 - delta);
+                        blue = zmianaKontrastuDlaDodatnich(c.B, 127 - delta);
+                    } 
+                    else
+                    {
+                        red = zmianaKontrastuDlaUjemnych(c.R, delta);
+                        green = zmianaKontrastuDlaUjemnych(c.G, delta);
+                        blue = zmianaKontrastuDlaUjemnych(c.B, delta);
+                    }
+                    
+                    zmodyfikowany.SetPixel(j, i, Color.FromArgb(c.A, red, green, blue));
+                }
+            }
+
+            pictureBox2.Image = zmodyfikowany;
+        }
+
+        private int zmianaKontrastuDlaDodatnich(int color, int delta)
+        {
+            if (color < 127) return (127 - delta) / 127 * color;
+            else return (127 - delta) / 127 * color + 2 * delta;
+        }
+
+        private int zmianaKontrastuDlaUjemnych(int color, int delta) 
+        {
+            if (color < 127 + delta) return (127 / (127 + delta)) * color;
+            else if (color > 127 - delta) return (127 * color + 255 * delta) / (127 + delta);
+            else return 127;
+        }
+
+
+        private void obliczHistogram()
+        {
+            int[] czerw = new int[256];
+            int[] ziel = new int[256];
+            int[] nieb = new int[256];
+
+            for(int i = 0; i < podstawa.Width; i++)
+            {
+                for(int j = 0; j < podstawa.Height; j++)
+                {
+                    Color pixel = zmodyfikowany.GetPixel(i, j);
+                    czerw[pixel.R]++;
+                    ziel[pixel.G]++;
+                    nieb[pixel.B]++;
+                }
+            }
+
+            chart1.Series["red"].Points.Clear();
+            chart1.Series["green"].Points.Clear();
+            chart1.Series["blue"].Points.Clear();
+            for (int i = 0; i < 256; i++)
+            {
+                chart1.Series["red"].Points.AddXY(i, czerw[i]);
+                chart1.Series["green"].Points.AddXY(i, czerw[i]);
+                chart1.Series["blue"].Points.AddXY(i, czerw[i]);
+            }
+            chart1.Invalidate();
+        }
+    }
+}
